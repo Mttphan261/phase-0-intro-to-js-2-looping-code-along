b6b70ff25ae48b8acb64e51247ad67b64999271c
Matt Phan <mttphan261@gmail.com>
2023-03-05 11:00:13 -0600

    commit tree 3c023aeccf539c04b4c59cbe8843ac874f165cd6

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+// Guadalupe, Ollie, Aki
+// Thank you, NAME, for the wonderful surprise gift! 
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+const cards =[]
+
+function writeCards(names) {
+    for (let i = 0; i <names.length; i++) {
+        cards[i]=`Thank you, ${names[i]}, for the wonderful surprise gift!`
+    }
+    return cards;
+    }
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number--);
+    }   
+    }
