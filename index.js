05a248d23a895cf892aed7219b6a9f03b5d85f90
Matt Phan <mttphan261@gmail.com>
2023-04-12 21:30:00 -0500

    completed lab again

--- index.js
+++ index.js
@@ -1,1 +1,124 @@
+const messages = [];
+
+function writeCards(array) {
+    for (let i = 0; i <array.length; i++) {
+        messages[i] = `Thank you, ${array[i]}, for the wonderful surprise gift!`;
+    }
+
+    return messages;
+
+
+} 
+
+function countDown(posInt) {
+    while (posInt > -1) {
+        console.log(posInt--);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const names = ["Guadalupe", "Ollie", "Aki"];
+// const cards =[]
+
+// function writeCards(names) {
+//     for (let i = 0; i <names.length; i++) {
+//         cards[i]=`Thank you, ${names[i]}, for the wonderful surprise gift!`
+//     }
+//     return cards;
+//     }
+
+
+
+
+// const gifts =["teddy bear", "drone", "doll"]
+
+// function wrapGifts(gifts) {
+//     for (let i = 0; i < gifts.length; i++) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`)
+//     }
+//     return gifts; 
+//     }
+
+// wrapGifts(gifts);
+
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Code your solutions in this file
+// Guadalupe, Ollie, Aki
+// Thank you, NAME, for the wonderful surprise gift! 
+
+
+
+
+
+
+
+
+
+
+
+// function countDown(number) {
+//     while (number >= 0) {
+//         console.log(number--);
+//     }   
+//     }
